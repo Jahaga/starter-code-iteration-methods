d18a96c98edabfe67996f902f4aa2ddfa637f91c
Tony <tony@email.local>
2020-07-03 15:05:52 -0500

    Assessment completed

--- main.js
+++ main.js
@@ -7,3 +7,48 @@ console.log(users)
 
 // Append the katas to this element:
 const main = document.querySelector('main')
+
+const greenEyes1 = users.filter(user => user.eyeColor === "green")
+printKata(0, greenEyes1)
+
+const userActive = users.filter(user => user.isActive === true)
+printKata(1, userActive)
+
+const usersEmail = users.map(user => user.email)
+printKata(2, usersEmail)
+
+const userOvation = users.some(user => user.company === "OVATION")
+printKata(3, userOvation)
+
+const userOver38 = users.find(user => user.age >= "38")
+printKata(4, userOver38)
+
+const userOver38Active = users
+    .filter(user => user.isActive === true)
+    .find(user => user.age >= "38")
+printKata(5, userOver38Active)
+
+const userBalance = users
+    .filter(user => user.company === "ZENCO")
+    .map(user => user.balance)
+printKata(6, userBalance)
+
+const userFugiat = users
+    .filter(user => user.tags.includes("fugiat"))
+    .map(user => user.age)
+printKata(7, userFugiat)
+
+function printKata(kataNumber, object) {
+    // For the usage of the DETAILS and SUMMARY tags
+    // in HTML, see: http://mdn.io/details-element
+    const detailsElement = document.createElement('details')
+    main.append(detailsElement)
+    //
+    const summaryElement = document.createElement('summary')
+    summaryElement.append("KATA " + kataNumber)
+    detailsElement.append(summaryElement)
+    //
+    // http://mdn.io/json.stringify
+    const stringifiedObject = JSON.stringify(object)
+    detailsElement.append(stringifiedObject)
+}
